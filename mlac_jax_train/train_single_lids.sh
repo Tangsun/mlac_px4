00a4c215d9768998ae0721bd2b72a9cf63588b12
tangsun <20776348+Tangsun@users.noreply.github.com>
2025-06-02 03:46:12 -0400

    add data

--- mlac_jax_train/train_single_lids.sh
+++ mlac_jax_train/train_single_lids.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 #SBATCH --job-name=train_3_on_gpu
 #SBATCH -o %j.log
-#SBATCH --partition=cpu
+#SBATCH --partition=cpu-gpu-rtx8000
 #SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=4
+#SBATCH --gres=gpu:1
+#SBATCH --mem=48G
 
 # Initialize the module command first source
 source /mnt/home/tangsun/miniconda3/etc/profile.d/conda.sh
